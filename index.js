292818b083a04bf7a81f1eba04cca250603083fd
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-27 22:04:04 +0600

    added env file setup and config mongo url

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// config
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +11,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// configuration
-require("dotenv").config();
+// mongodb
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@atlascluster.xgsegjb.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const productCollection = client
+      .db("artcraftDB")
+      .collection("all products");
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("server is running");
